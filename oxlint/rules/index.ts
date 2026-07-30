a0eeb82d20f1fc99fca3e3bb39390fe4ef2c149f
Harry Solovay <harrysolovay@gmail.com>
2026-07-30 14:29:28 -0400

    crosshatch module boundary lint

--- oxlint/rules/index.ts
+++ oxlint/rules/index.ts
@@ -1,3 +1,4 @@
+import { crosshatchEnforcePackageImportBoundaries } from "./crosshatch_enforce_package_import_boundaries.ts"
 import { noPackageSelfImport } from "./no_package_self_import.ts"
 import { requireEffectAllExplicitConcurrency } from "./require_effect_all_explicit_concurrency.ts"
 import { requireReadonlyTypeMembers } from "./require_readonly_type_members.ts"
@@ -5,6 +6,7 @@ import { requireReadonlyTypeMembers } from "./require_readonly_type_members.ts"
 export default {
   meta: { name: "custom" },
   rules: {
+    "crosshatch-enforce-package-import-boundaries": crosshatchEnforcePackageImportBoundaries,
     "no-package-self-import": noPackageSelfImport,
     "require-effect-all-explicit-concurrency": requireEffectAllExplicitConcurrency,
     "require-readonly-type-members": requireReadonlyTypeMembers,
